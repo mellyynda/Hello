16dfa58da0a766a3872392c4963ebdb1fe2dedaa
Melinda <melinda.sava.td@gmail.com>
2021-02-18 22:35:27 +0100

    fixed random quotes API on session reload

--- index.js
+++ index.js
@@ -7,19 +7,12 @@ const clickMe = document.querySelector('.cover div span');
 
 let user = '';
 let firstVisit = 0;
-let quoteDay = null;
 
 const getQuotes = async () => {
     const quotes = await fetch('https://type.fit/api/quotes');
     const quotesObj = await quotes.json();
     return quotesObj;
 }
-
-function randomIntFromInterval(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-
 
 document.addEventListener('DOMContentLoaded', () => {
     checkUser();
@@ -28,38 +21,48 @@ document.addEventListener('DOMContentLoaded', () => {
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     user = event.target.querySelector('input[type=text]').value;
+
     if (user.trim()) {
         localStorage.setItem('user', user);
         firstVisit++;
         localStorage.setItem('firstVisit', firstVisit);
+
         getQuotes()
             .then(quotesObj => {
                 const rand = randomIntFromInterval(0, 1643);
-                const quote = quotesObj[rand].text;
-                messageWrapper.innerHTML = setMessage('Nice to meet you', user, quote);
+                messageWrapper.innerHTML = setMessage('Nice to meet you', user, quotesObj[rand].text, quotesObj[rand].author);
             });
+
         form.classList.add('hidden');
         callToAct.classList.add('hidden');
     } else messageWrapper.innerHTML = `error please provide a name`;
 });
 
-const setMessage = (message, user, quote) => {
-    quoteDay = new Date();
-    return `<p class="greeting">${message} ${user}!</p><p>Quote of the day:</p><h2><q>${quote}</q></h2>`
+const setMessage = (message, user, quote, author) => {
+    return `<p class="greeting">${message} ${user}!</p><h2><q>${quote}</q></h2><p>${author}</p>`
 }
 
 const checkUser = () => {
     firstVisit = localStorage.getItem('firstVisit');
+
     if (firstVisit == 1) {
         console.log('visit is 1');
         form.classList.add('hidden');
         callToAct.classList.add('hidden');
-        messageWrapper.innerHTML = setMessage('Welcome back', localStorage.getItem('user'));
+
+        getQuotes()
+            .then(quotesObj => {
+                const rand = randomIntFromInterval(0, 1643);
+                messageWrapper.innerHTML = setMessage('Welcome back', localStorage.getItem('user'), quotesObj[rand].text, quotesObj[rand].author);
+            });
+
         animateTitle();
     }
 }
 
-
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 const animateTitle = () => {
     title.classList.toggle('clicked');
